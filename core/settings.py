bf99f241e21cf523d4eee38589da8513006d1cde
siddesh thotam <siddeshthotam310@gmail.com>
2025-08-17 16:36:08 +0530

    fixed the colors

--- core/settings.py
+++ core/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-a(z_@m(_1aa&+7@i$sk=_=u-+q-3i=zsh^70@n(vcp_d)$&+pu
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
